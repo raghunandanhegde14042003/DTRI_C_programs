d06c9ee8ef63f7fad5d394281e326024c7a497f2
raghunandanhegde14042003 <raghunandanhegde14@gmail.com>
2025-01-02 18:50:26 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a=2,b=2;
+    int x,y;
+    x=a++;
+    y=++b;
+    printf("%d\n",x);
+    printf("%d\n",y);
+    printf("present value of a is %d\n",a);
+    printf("present value of b is %d\n",b);
+
+    return 0;
+}
